368d76e57bdc497954bde09a1776b176d0f38267
Chinmay Anaokar <chinmay8116@gmail.com>
2021-08-27 08:16:19 -0700

    commit-heap sort

--- Heap Sort/Program.cs
+++ Heap Sort/Program.cs
@@ -1,15 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Heap_Sort
+﻿namespace Heap_Sort
 {
+
+    public static class AppHelper
+    {
+        public static void BuildHeap(ref int[] arr, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(ref arr, n, i);
+        }
+        public static void sort(ref int[] arr)
+        {
+            int n = arr.Length;
+
+            BuildHeap(ref arr, n);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                heapify(ref arr, i, 0);
+            }
+        }
+        public static void heapify(ref int[] arr, int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                heapify(ref arr, n, largest);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[] { 10, 15, 50, 4, 20 };
+            AppHelper.sort(ref arr);
         }
     }
 }
